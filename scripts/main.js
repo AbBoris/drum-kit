e2bfcb168db1ef7fcc9b21e28fe3ac8d98bb6bb0
boris abdulov <abdulov.b@gmail.com>
2017-06-16 01:30:10 +0300

    allow the user to record sounds

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,68 @@ const keys = Array.from(document.querySelectorAll('.keys__item'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
 
+const recordButton = document.querySelector('.record-panel__record-button');
+
+(function () {
+    let record = [];
+    let isRecord = false;
+    let lastSoundTime = 0;
+
+    recordButton.addEventListener('click', (e) => {
+        isRecord = !isRecord;
+        if (isRecord) {
+            recordButton.classList.remove('record-panel__record-button--play');
+            recordButton.classList.add('record-panel__record-button--stop');
+            startRecord();
+        } else {
+            recordButton.classList.remove('record-panel__record-button--stop');
+            recordButton.classList.add('record-panel__record-button--play');
+            stopRecord();
+        }
+    });
+
+    function startRecord() {
+        record = [];
+        lastSoundTime = 0;
+        window.addEventListener('keydown', addSound);
+    }
+
+    function stopRecord() {
+        window.removeEventListener('keydown', addSound);
+        console.log(record);
+        playRecord();
+    }
+
+    function addSound(event) {
+        let interval = 0;
+
+        if (lastSoundTime) {
+            interval = event.timeStamp - lastSoundTime;
+        }
+        lastSoundTime = event.timeStamp;
+
+        record.push({ event, interval });
+    }
+
+    function playRecord() {
+        let recordIndex = 0;
+
+        playSounds();
+
+        function playSounds() {
+            if (recordIndex < record.length) {
+                playSoundPromise(record[recordIndex++]).then(playSounds);
+            }
+        };
+
+        function playSoundPromise(sound) {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    playSound(sound.event);
+                    resolve();
+                }, sound.interval);
+            });
+        }
+    }
+
+}());
